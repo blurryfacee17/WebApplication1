5d8a06fd1bc25bca88611c5fc080cd58e9412fb3
blurryfacee17 <curvepiece@gmail.com>
2022-10-21 13:53:55 +0400

    Initial commit

---
+++ RazorPages/ViewModels/ProductAdditionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPages.ViewModels;
+
+public class ProductAdditionModel
+{
+    [Required]
+    public string Name { get; set; }
+    [Range(1,1_000_000)]
+    public decimal Price { get; set; }
+    [Range(0,int.MaxValue)]
+    public int Stock { get; set; }
+    [Required]
+    public string ImageSource { get; set; }
+    [Required]
+    public string Category { get; set; }
+    [Required]
+    public string Description { get; set; }
+    
+}
