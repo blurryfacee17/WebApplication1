77abbfd0dfb25fc539198d14021470fe091f097e
blurryfacee17 <curvepiece@gmail.com>
2022-10-28 16:34:12 +0400

    Initial Commit

--- SmtpPractice/ServiceStartupNotificationBackgroundService.cs
+++ SmtpPractice/ServiceStartupNotificationBackgroundService.cs
@@ -2,17 +2,29 @@
 
 public class ServiceStartupNotificationBackgroundService: BackgroundService
 {
-    private readonly IEmailSender _emailSender;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
 
     public ServiceStartupNotificationBackgroundService(IServiceScopeFactory serviceScopeFactory)
     {
-        var scope = serviceScopeFactory.CreateScope();
-        _emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
+        _serviceScopeFactory = serviceScopeFactory;
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _emailSender.Send("danildudyrev@mail.ru", "Сервер запущен", "Сервер успешно запущен");
+        using var scope1 = _serviceScopeFactory.CreateScope();
+        using var scope2 = _serviceScopeFactory.CreateScope();
+        var emailSender1 = scope1.ServiceProvider.GetRequiredService<IEmailSender>();
+        var emailSender2 = scope2.ServiceProvider.GetRequiredService<IEmailSender>();
+        Parallel.Invoke(() => 
+            emailSender1.Send(
+                "danildudyrev@mail.ru",
+                "Сервер запущен", 
+                "Сервер успешно запущен"),
+            () => emailSender2.Send(
+                "danildudyrev@mail.ru",
+                "Сервер запущен", 
+                "Сервер успешно запущен")
+            );
     }
     
 }
